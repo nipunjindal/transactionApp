e6fcf2494e9d9c30b07c87b8820246556f962c8b
njindal <njindal@adobe.com>
2018-03-10 09:41:44 +0530

    initial commit

---
+++ expenseSharingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  expenseSharingApp
+//
+//  Created by njindal on 3/10/18.
+//  Copyright © 2018 adobe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
